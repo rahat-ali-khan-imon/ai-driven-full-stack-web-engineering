6bc754d018a388747841907b5168be32f38ef4d5
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 04:40:46 +0600

    Added parseInt and parseFloat examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m07.03_parseInt.js
@@ -0,0 +1,20 @@
+const number = 20;
+console.log(typeof number);
+console.log();
+
+
+const numberString1 = '20';
+console.log(typeof numberString1);
+console.log();
+
+
+const numberString2 = '20';
+const num1 = parseInt(numberString2);            // String to Number Convert
+console.log(typeof num1);
+console.log();
+
+
+const numberString3 = '20.55';
+const num2 = parseInt(numberString3);
+
+console.log(typeof num2);
---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m07.04_parseFloat.js
@@ -0,0 +1,25 @@
+const number = 20;
+console.log(number, '--->', typeof number);
+console.log();
+
+
+const numberString1 = '20';
+console.log(numberString1, '--->', typeof numberString1);
+console.log();
+
+
+const numberString2 = '20';
+const num1 = parseInt(numberString2);            // String to Number Convert
+console.log(num1, '--->', typeof num1);
+console.log();
+
+
+const numberString3 = '20.55';
+const num2 = parseInt(numberString3);
+console.log(num2, '--->', typeof num2);
+console.log();
+
+
+const numberString4 = '20.55';
+const num3 = parseFloat(numberString4);
+console.log(num3, '--->', typeof num3);
