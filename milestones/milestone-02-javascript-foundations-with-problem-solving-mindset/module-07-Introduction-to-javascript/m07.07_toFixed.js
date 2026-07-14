d8d6f9ee11f2b1fdeff6bd64387ba5be932b3f64
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 04:45:53 +0600

    Added toFixed examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m07.07_toFixed.js
@@ -0,0 +1,7 @@
+const firstNumber = 0.1;
+const secondNumber = 0.2;
+
+const total = firstNumber + secondNumber;
+const totalTwoDecimal = total.toFixed(2);
+
+console.log(totalTwoDecimal, '--->', typeof totalTwoDecimal);
