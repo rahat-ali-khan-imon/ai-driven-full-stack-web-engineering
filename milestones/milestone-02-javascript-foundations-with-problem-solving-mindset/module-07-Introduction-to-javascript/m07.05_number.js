7cbdc2e97016cb0701419ae9a5bee29909a43fa6
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 04:44:06 +0600

    Added parseInt conversion examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m07.05_number.js
@@ -0,0 +1,13 @@
+const numberString1 = 'jenniejisoolalisarose';
+console.log(numberString1, '--->', typeof numberString1);
+console.log();
+
+
+const number1 = parseInt(numberString1)
+console.log(number1, '--->', typeof numberString1);
+console.log()
+
+
+const numberString2 = '12345jenniejisoolalisarose';
+const number2 = parseInt(numberString2)
+console.log(number2, '--->', typeof numberString2);
