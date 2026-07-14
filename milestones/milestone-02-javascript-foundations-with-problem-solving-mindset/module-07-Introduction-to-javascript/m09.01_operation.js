a4a4e916138369eda6f5d4826d4aa82f70309377
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 05:18:30 +0600

    Added arithmetic operation examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m09.01_operation.js
@@ -0,0 +1,15 @@
+console.log(50 + 24);
+console.log(100 - 245);
+console.log();
+
+
+const shirtPrice = 300;
+const pantPrice = 500;
+console.log(pantPrice - shirtPrice);
+console.log();
+
+
+const mangoPricePerKg = 100;
+const mangoQuantityInKg = 5;
+const payableAmount = mangoPricePerKg * mangoQuantityInKg;
+console.log(payableAmount);
