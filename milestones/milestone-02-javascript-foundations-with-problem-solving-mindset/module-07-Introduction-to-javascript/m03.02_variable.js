5ce40ac3857c813a2b008e85352ac651914c783e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 02:12:46 +0600

    Implemented var, let, and const examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m03.02_variable.js
@@ -0,0 +1,32 @@
+// Variable
+var weight = 38;
+var temperature = 39;
+var people = 41;
+
+console.log(temperature);
+
+
+// let, const
+let price = 30;
+console.log(price);
+
+price = 50;
+price = 200;
+
+console.log(price);
+
+
+const PI = 3.14;
+console.log(PI);
+
+// PI = 123.321;
+// console.log(PI);
+
+
+let studentName = 'Alice';
+console.log(studentName);
+
+
+let score = 0;
+score = 97;
+console.log(score);
