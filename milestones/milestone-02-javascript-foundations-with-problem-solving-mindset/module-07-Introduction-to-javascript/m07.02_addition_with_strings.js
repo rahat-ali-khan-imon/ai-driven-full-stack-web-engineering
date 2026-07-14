e5cb48db6e8fff64c17a044c71e9a20d1e08ba6e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 04:38:06 +0600

    Added string concatenation examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m07.02_addition_with_strings.js
@@ -0,0 +1,15 @@
+const orangePrice = 20;
+const mangoPrice = '35';
+const totalPrice = orangePrice + mangoPrice;
+
+console.log(orangePrice, '---->', typeof orangePrice);
+console.log(mangoPrice, '----->', typeof mangoPrice);
+console.log(totalPrice, '----->', typeof totalPrice);
+console.log();
+
+
+const firstName = 'Jisoo';
+const secondName = 'Lalkim';
+const fullName = firstName + secondName;
+
+console.log(fullName);
