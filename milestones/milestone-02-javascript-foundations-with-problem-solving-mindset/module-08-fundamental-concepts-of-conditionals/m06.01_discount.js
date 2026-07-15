16b34039e76b964b1af93024e914ee65b37c4ebe
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-15 20:46:17 +0600

    Implemented multi-level if-else examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m06.01_discount.js
@@ -0,0 +1,13 @@
+const totalAmount = 5000;
+
+if(totalAmount > 5000) {
+    const discount = 30;
+    const discountAmount = totalAmount / 100 * discount;
+    const payment = totalAmount - discountAmount;
+    console.log(payment);
+} else if(totalAmount > 1000) {
+    const discount = 10;
+    const discountAmount = totalAmount / 100 * discount;
+    const payment = totalAmount - discountAmount;
+    console.log('Please pay: ', payment);
+}
