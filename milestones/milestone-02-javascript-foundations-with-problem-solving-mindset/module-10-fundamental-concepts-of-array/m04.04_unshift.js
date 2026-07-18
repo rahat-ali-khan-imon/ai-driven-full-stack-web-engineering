77f91fceadc81bb11f31350e38eed80218fa7cb8
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-19 00:13:59 +0600

    Implemented array unshift examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m04.04_unshift.js
@@ -0,0 +1,8 @@
+const friends = ['Balam', 'Kalam', 'Salam', 'Gelam', 'Khailam'];
+console.log(friends);
+
+friends.unshift('Ghumailam');
+console.log(friends);
+
+friends.unshift('mobailam');
+console.log(friends);
