709134c91ddd0419ea8c13f7b3c6158fdfedc2e8
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-18 23:32:56 +0600

    Implemented array pop examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m04.02_pop.js
@@ -0,0 +1,17 @@
+const numbers = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+
+const firstOut = numbers.pop();
+const secondOut = numbers.pop();
+
+console.log(firstOut, secondOut);
