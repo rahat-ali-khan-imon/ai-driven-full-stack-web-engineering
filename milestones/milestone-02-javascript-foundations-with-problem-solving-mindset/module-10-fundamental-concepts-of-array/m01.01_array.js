df88658a7c15be442f38ef307e44cab8e928b41d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-18 20:50:17 +0600

    Implemented arrays for storing multiple values

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m01.01_array.js
@@ -0,0 +1,14 @@
+// Variable can hold only one value and one data
+const player1 = 45;
+const player2 = 98;
+const player3 = 56;
+const player4 = 45;
+const player5 = 98;
+const player6 = 56;
+
+
+
+// Array can hold more than one value
+const teams = [123, 111, 333, 443, 235];
+const players = ['Alice', 'Jennie', 'Jisoo', 'Lisa', 'Rose'];
+const richStudents = [true, false, true, false, true];
