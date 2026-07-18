d231c289c5749ad98fede1d9205436901828314d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-18 23:07:52 +0600

    Implemented array access and update examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m03.02_index.js
@@ -0,0 +1,11 @@
+const player = ['Alice', 'Jennie', 'Babul', 'Srabony', 'Jerin'];
+
+console.log(player.length);
+
+const player1 = player[0];
+console.log(player1);
+console.log(player[player.length - 1]);
+
+// Value set
+player[2] = 'La Lisa';
+console.log(player);
