a033cd35f5e8f7cafe140735643dde506513ae39
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-19 00:21:11 +0600

    Implemented array includes examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m05.01_includes.js
@@ -0,0 +1,13 @@
+const numbers = [10, 20, 30, 40, 50];
+
+const hasItem = numbers.includes(100);
+console.log(hasItem);
+
+
+const friends = ['Balam', 'Kalam', 'Salam', 'Gelam', 'Khailam'];
+
+if(friends.includes('balam')) {
+    console.log('balam is playling');
+} else {
+    console.log('Not playing');
+}
