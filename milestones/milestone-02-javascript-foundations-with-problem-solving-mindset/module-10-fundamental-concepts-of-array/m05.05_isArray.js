77012d77b4e52c8d50ea34a509a01d4f8de74ba1
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-19 00:47:09 +0600

    Added Array.isArray examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m05.05_isArray.js
@@ -0,0 +1,9 @@
+const numbs = [10, 20, 30, 40, 50];
+
+console.log(typeof numbs);
+console.log();
+
+
+console.log(Array.isArray(numbs));
+console.log(Array.isArray(45));
+console.log(Array.isArray([45]));
