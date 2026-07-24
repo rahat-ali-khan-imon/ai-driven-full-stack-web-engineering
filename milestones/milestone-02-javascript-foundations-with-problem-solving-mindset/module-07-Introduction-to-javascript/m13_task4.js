b1bcf8f87bdfa64d3931698cef931325e5e10ef2
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 03:07:15 +0600

    Implemented data type checking using typeof

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m13_task4.js
@@ -0,0 +1,15 @@
+/* Task 4 — Identify Data Types
+Use the typeof operator to check the data type of each variable.
+*/
+
+let a = 100;
+let b = 'Programming';
+let c = true;
+let d = false;
+let e = 3.1416;
+
+console.log('a:', typeof a);
+console.log('b:', typeof b);
+console.log('c:', typeof c);
+console.log('d:', typeof d);
+console.log('e:', typeof e);
