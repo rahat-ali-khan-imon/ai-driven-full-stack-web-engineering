b8c61a7eef1aeb0250fb07f5cf8130c78f694f9e
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 03:24:30 +0600

    Implemented personal bio using variables

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m15_task6.js
@@ -0,0 +1,18 @@
+/* Task 6 — Personal Bio
+Store personal details and print a friendly, formatted introduction.
+*/
+
+
+let name = "Alex";
+let age = 20;
+let favoriteFood = "Pizza";
+let favoriteLanguage = "JavaScript";
+let lovesCoding = true;
+
+ 
+
+console.log("Hello!");
+console.log("My name is " + name + ".");
+console.log("I am " + age + " years old.");
+console.log("My favorite food is " + favoriteFood + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
