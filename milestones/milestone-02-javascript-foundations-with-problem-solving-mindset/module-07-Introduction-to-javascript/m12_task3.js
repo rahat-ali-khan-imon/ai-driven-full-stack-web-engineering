5d3f7246865435608628a1ee44b685f515681853
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 03:01:28 +0600

    Added student information using variables

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m12_task3.js
@@ -0,0 +1,14 @@
+/* Task 3 — Student Information
+Store student details in variables of the appropriate type and print them.
+*/
+
+
+let studentName = 'Alex Johnson';
+let age = 20;
+let country = 'Bangladesh';
+let isStudent = true;
+
+console.log('Student Name:', studentName);
+console.log('Age:', age);
+console.log('Country: ', country);
+console.log('Is Student: ', isStudent);
