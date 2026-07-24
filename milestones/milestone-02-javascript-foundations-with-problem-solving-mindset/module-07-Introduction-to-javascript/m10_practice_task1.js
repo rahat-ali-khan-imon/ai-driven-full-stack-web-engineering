4167d82f69befc8c693fc3c58035e6d6360ca154
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 02:38:12 +0600

    Added first JavaScript program

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m10_practice_task1.js
@@ -0,0 +1,9 @@
+/* Task 1 — Your First JavaScript Program
+
+Create a file named app.js and print three introductory messages.
+*/
+
+
+console.log('Hello, JavaScript!');
+console.log('My name is Alex.');
+console.log('I am excited to become a Web Developer.');
