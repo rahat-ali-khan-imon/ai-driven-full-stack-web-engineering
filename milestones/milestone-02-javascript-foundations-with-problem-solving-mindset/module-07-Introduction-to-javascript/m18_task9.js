d6fb20b1fbea91df4716131e5e32939d95310325
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 15:06:10 +0600

    Implemented purchase price and discount calculation

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m18_task9.js
@@ -0,0 +1,17 @@
+/* Task 9 — Mini Coding Challenge
+Calculate the total price, discount amount, and final price for a purchase.
+*/
+
+let price = 200;
+let quantity = 3;
+let discount = 10; // percent
+ 
+
+let totalPrice = price * quantity;
+let discountAmount = (totalPrice * discount) / 100;
+let finalPrice = totalPrice - discountAmount;
+ 
+
+console.log("Total Price:", totalPrice);
+console.log("Discount Amount:", discountAmount);
+console.log("Final Price:", finalPrice);
