3bf4a37aa3f0fad6a3995a49f043437f06d5a8d8
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 16:55:15 +0600

    Implemented comparison operator examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m01_practice_task1.js
@@ -0,0 +1,16 @@
+/*
+Take two variables price1 and price2, and compare them using all comparison operators 
+(==, ===, !=, !==, >, <, >=, <=), printing each result with console.log.
+*/
+
+const price1 = 100;
+const price2 = 200;
+
+console.log("price1 == price2:", price1 == price2);
+console.log("price1 === price2:", price1 === price2);
+console.log("price1 != price2:", price1 != price2);
+console.log("price1 !== price2:", price1 !== price2);
+console.log("price1 > price2:", price1 > price2);
+console.log("price1 < price2:", price1 < price2);
+console.log("price1 >= price2:", price1 >= price2);
+console.log("price1 <= price2:", price1 <= price2);
