c4e02a3fb43917cd3ee454950fd23787f6b22baf
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 22:14:15 +0600

    Implemented access control using logical NOT operator

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m19_task19.js
@@ -0,0 +1,10 @@
+// Using hasPermission, use the ! operator to print "Access Denied" if permission is not granted.
+
+
+const hasPermission = false;
+
+if (!hasPermission) {
+    console.log("Access Denied");
+} else {
+    console.log("Access Granted");
+}
