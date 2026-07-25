3ac95d86927ada4fe1a9c1b6f59a766aa3e7f683
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 17:05:50 +0600

    Implemented simple if condition example

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m03_task3.js
@@ -0,0 +1,17 @@
+/*
+Create a variable isRaining (true/false). If it is true, print "Take an umbrella".
+*/
+
+let isRaining = true;
+
+if(isRaining) {
+    console.log('Take an Umbrella.');
+}
+
+
+
+const isRaining2 = false;
+
+if(isRaining2) {
+    console.log('Take an Umbrella.');
+}
