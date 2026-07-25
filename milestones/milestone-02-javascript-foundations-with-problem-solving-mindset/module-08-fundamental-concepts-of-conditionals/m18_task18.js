72a07342138e71403ac2c996aad8571627ac739c
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 22:07:36 +0600

    Added ternary operator price comparison example

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m18_task18.js
@@ -0,0 +1,11 @@
+/*
+If the price is greater than 1000, print "Expensive", otherwise "Affordable" — write this using a ternary
+operator.
+*/
+
+
+const price = 1200;
+
+const result = (price > 1000) ? "Expensive" : "Affordable";
+
+console.log(result);
