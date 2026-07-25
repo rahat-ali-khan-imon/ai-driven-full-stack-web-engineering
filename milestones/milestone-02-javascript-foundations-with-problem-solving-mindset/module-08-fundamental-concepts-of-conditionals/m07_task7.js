704d276b07c77e14f8b0dacafb6e0bbfd1868f4b
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 17:24:13 +0600

    Implemented speed limit check using if-else

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m07_task7.js
@@ -0,0 +1,10 @@
+// Take a speed variable. If speed is greater than 80, print "OverSpeeding", otherwise print "Normal speed".
+
+
+const speed = 100;
+
+if(speed > 80) {
+    console.log('OverSpeeding');
+} else {
+    console.log('Normal Speed');
+}
