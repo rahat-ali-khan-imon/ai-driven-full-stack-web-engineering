447072287b7de2e8ba13adff0c262cc5071adac2
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 12:35:11 +0600

    Implemented arithmetic operations practice

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-07-Introduction-to-javascript/m16_task7.js
@@ -0,0 +1,17 @@
+/* Task 7 — Arithmetic Practice
+Take two numbers and print the results of all five arithmetic operations, matching the example format.
+*/
+
+let number1 = 50;
+let number2 = 8;
+
+ 
+
+console.log("Number 1:", number1);
+console.log("Number 2:", number2);
+console.log();
+console.log("Addition:", number1 + number2);
+console.log("Subtraction:", number1 - number2);
+console.log("Multiplication:", number1 * number2);
+console.log("Division:", number1 / number2);
+console.log("Modulus:", number1 % number2);
