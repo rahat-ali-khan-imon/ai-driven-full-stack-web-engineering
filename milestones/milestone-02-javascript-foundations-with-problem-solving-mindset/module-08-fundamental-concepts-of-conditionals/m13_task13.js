50c455a8134daea2c272afb71b5c883288b9185d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 18:52:04 +0600

    Implemented season detection using month and if-else if

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m13_task13.js
@@ -0,0 +1,18 @@
+// Using a month number (1-12), determine which season that month falls in (Winter, Summer, Monsoon, etc.).
+
+
+const month = 7;
+
+if(month === 12 || month === 1 || month === 2) {
+    console.log("Winter");
+} else if(month === 3 || month === 4 || month === 5) {
+    console.log("Summer");
+} else if(month === 6 || month === 7 || month === 8) {
+    console.log("Monsoon");
+} else if(month === 9 || month === 10) {
+    console.log("Autumn");
+} else if(month === 11) {
+    console.log("Late Autumn");
+} else{
+    console.log("Invalid month");
+}
