1268b3a57698f488cde9cddd098f2f9255416bbe
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 18:33:03 +0600

    Implemented grading system using if-else if ladder

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m11_task11.js
@@ -0,0 +1,15 @@
+// Build a grading system using a marks variable (A+, A, B, C, F) — it must have at least 5 condition branches.
+
+const marks = 85;
+
+if (marks >= 80) {
+    console.log("Grade: A+");
+} else if (marks >= 70) {
+    console.log("Grade: A");
+} else if (marks >= 60) {
+    console.log("Grade: B");
+} else if (marks >= 50) {
+    console.log("Grade: C");
+} else {
+    console.log("Grade: F");
+}
