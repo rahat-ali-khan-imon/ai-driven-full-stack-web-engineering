22df9f5672da77b6ababa7b1de3b658781464ed0
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 17:30:29 +0600

    Implemented entry eligibility check using logical AND

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m08_task8.js
@@ -0,0 +1,10 @@
+// Using age and hasTicket, print "Entry allowed" if age is above 18 AND the person has a ticket (use &&).
+
+const age = 23;
+const hasTicket = true;
+
+if(age > 18 && hasTicket) {
+    console.log('Entry allowed');
+} else {
+    console.log('Entry denied');
+}
