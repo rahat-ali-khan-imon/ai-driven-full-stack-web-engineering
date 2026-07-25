03aab05e475ce802584c25bc77423e8f25937385
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 22:16:32 +0600

    Implemented online status check using logical NOT operator

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m20_task20.js
@@ -0,0 +1,10 @@
+// Using isOnline, use the NOT operator to print "User is offline".
+
+
+const isOnline = false;
+
+if (!isOnline) {
+    console.log("User is offline");
+} else {
+    console.log("User is online");
+}
