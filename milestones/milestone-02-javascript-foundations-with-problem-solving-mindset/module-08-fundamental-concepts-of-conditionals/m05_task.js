01d4080925c32c7f80fc5075dfe60e35d57d7558
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 17:15:18 +0600

    Implemented positive and negative number check using if-else

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m05_task.js
@@ -0,0 +1,9 @@
+// Take a number variable and check whether it is positive or negative (using if-else).
+
+const number = -10;
+
+if(number > 0) {
+    console.log('Positive');
+} else {
+    console.log('Negative');
+}
