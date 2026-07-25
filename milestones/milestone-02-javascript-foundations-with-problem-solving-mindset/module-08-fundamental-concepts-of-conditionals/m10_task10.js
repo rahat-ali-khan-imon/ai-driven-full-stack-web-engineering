7b5ca014c4523398d063f2c818af392c308633fd
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-25 18:11:35 +0600

    Implemented simple login authentication using if-else

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-08-fundamental-concepts-of-conditionals/m10_task10.js
@@ -0,0 +1,10 @@
+// Build a simple login system — print "Login successful" only if both username and password are correct.
+
+const username = 'admin';
+const password = '1234';
+
+if(username == 'admin' && password == '1234') {
+    console.log('Login Successful.');
+} else {
+    console.log('Invalid username or password.');
+}
