6b0d955cc448caa900d0e9b46d60d6c5d376553d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-26 01:46:41 +0600

    Implemented even and odd check without function

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-13-basic-problem-solving-with-javascript/m02.01_EvenOdd.js
@@ -0,0 +1,21 @@
+// Problem 01: Check if a number is Even or Odd
+
+let number = 10;
+
+// Without function
+if(number % 2 === 0) {
+    console.log('Even Number');
+} else {
+    console.log('Odd Number');
+}
+console.log();
+
+
+
+number = 21;
+
+if(number % 2 === 0) {
+    console.log('Even Number');
+} else {
+    console.log('Odd Number');
+}
