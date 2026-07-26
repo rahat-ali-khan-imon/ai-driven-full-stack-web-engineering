a68315cbbedd06a8f4e83947d0a21da7f0677069
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-26 19:34:44 +0600

    Implemented do...while loop execution examples

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-09-fundamental-concepts-of-loops/m13_task12.js
@@ -0,0 +1,20 @@
+/* Task 12 — Do While Loop — Optional 
+Write a do while loop that prints numbers from 1 to 5. Then modify the condition so it is false 
+from the start (for example, i > 10) and observe that the loop still runs once.
+*/
+
+let i = 1; 
+
+do {
+    console.log(i);;
+    i++;
+} while(i <= 5);
+console.log('\n');
+
+
+i = 1;
+
+do {
+    console.log(i);
+    i++;
+} while (i > 10);
