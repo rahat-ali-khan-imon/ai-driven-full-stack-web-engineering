d63c00b84f9c150993e10ea1c75c8e075c423463
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 00:59:04 +0600

    Added no-error-thrown example for type coercion behavior

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-14-debugging-and-problem-solving-with-javascript/m01.10_NoErrorThrown.js
@@ -0,0 +1,10 @@
+let price = '10';
+let quantity = 5;
+
+console.log(price * quantity);
+console.log();
+
+
+
+let total = '10' + 5;     // glues text
+console.log(total);     // the silent bug or logical error
