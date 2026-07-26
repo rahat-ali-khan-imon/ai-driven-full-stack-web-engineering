2901ed709b9308fdf6c7738456d844f4d6773e1d
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-26 18:10:13 +0600

    Implemented number printing from 1 to 10 using for loop

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-09-fundamental-concepts-of-loops/m02_task2.js
@@ -0,0 +1,16 @@
+/* Task 2 — Print Numbers 1 to 10 
+Write a for loop that prints numbers from 1 to 10.
+*/
+
+for(let i = 1; i <= 10; i++) {
+    console.log(i, ' ');
+}
+console.log();
+
+
+let result = '';
+
+for(let i = 1; i <= 10; i++) {
+    result += i + ' ';
+    console.log(result);
+}
