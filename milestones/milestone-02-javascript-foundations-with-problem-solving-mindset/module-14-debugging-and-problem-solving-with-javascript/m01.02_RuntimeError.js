68549f740a5e7f28b36afe2b41cfea5695c8471c
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 00:05:36 +0600

    Added runtime error example for debugging practice

--- milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-14-debugging-and-problem-solving-with-javascript/m01.02_RuntimeError.js
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-14-debugging-and-problem-solving-with-javascript/m01.02_RuntimeError.js
@@ -1,3 +1,3 @@
-// let numbers = [1, 2, 3];
+// let numbers = [1, 2, 3];             // RuntimeError: Runs, then crashes
 
 // console.log(numbers.push(4).toUpperCase());     // push() returns a numbers
