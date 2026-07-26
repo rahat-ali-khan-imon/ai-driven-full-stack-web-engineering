f1e952c4927031c2924dcd4ac7e2de9b8ebe2757
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-26 18:04:16 +0600

    Implemented for loop to print 'Hello' five time

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-09-fundamental-concepts-of-loops/m01_practice_task1.js
@@ -0,0 +1,12 @@
+/* Task 1 — Print "Hello" Five Times
+Write a for loop that prints "Hello" 5 times.
+*/
+
+for(let i = 1; i <= 5; i++) {
+    console.log('Hello');
+}
+console.log('\n');
+
+for(let i = 0; i < 5; i++) {
+    console.log('Hello');
+}
