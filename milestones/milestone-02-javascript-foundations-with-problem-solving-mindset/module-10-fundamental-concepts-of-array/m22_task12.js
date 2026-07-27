ce6a769e993444abfd772d6b4b844afab239cb8a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 19:57:31 +0600

    Looped through the array using a while loop

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m22_task12.js
@@ -0,0 +1,10 @@
+// 12. Loop through the array with a while loop and log each item.
+
+const fruits = ["Apple", "Banana", "Mango"];
+
+let i = 0;
+
+while (i < fruits.length) {
+    console.log(fruits[i]);
+    i++;
+}
