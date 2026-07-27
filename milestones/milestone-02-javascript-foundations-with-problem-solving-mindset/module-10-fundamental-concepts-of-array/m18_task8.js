1f19de51be8c4dd84538390ac4eca53e58cfd427
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 19:49:26 +0600

    Removed the first item from the array

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m18_task8.js
@@ -0,0 +1,8 @@
+// 8. Remove the first item from the array. Log the array and the removed value.
+
+const fruits = ["Apple", "Banana", "Mango"];
+
+const removedItem = fruits.shift();
+
+console.log(fruits);
+console.log(removedItem);
