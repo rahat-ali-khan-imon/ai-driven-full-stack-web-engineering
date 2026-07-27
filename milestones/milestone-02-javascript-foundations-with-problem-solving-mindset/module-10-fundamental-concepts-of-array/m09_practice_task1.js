2eeb8d869f99c8149ea1378938b326410812173f
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 19:19:34 +0600

    Implemented array creation and length retrieval

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m09_practice_task1.js
@@ -0,0 +1,6 @@
+/* 1. Create an array with 3 items of your choice. Log it.
+Expected: array with your 3 items printed
+*/
+
+const fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits);
---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m10_task2.js
@@ -0,0 +1,6 @@
+/* 2. Log the length of the array from Task 1.
+Expected: 3
+*/
+
+const fruits = ["Apple", "Banana", "Mango"];
+console.log(fruits.length);
