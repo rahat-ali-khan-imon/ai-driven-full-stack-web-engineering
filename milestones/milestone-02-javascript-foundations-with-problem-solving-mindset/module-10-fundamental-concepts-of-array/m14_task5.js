1bf91e0065c173073071ca96bb13842d924decc1
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 19:39:01 +0600

    Implemented array item addition

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m14_task5.js
@@ -0,0 +1,7 @@
+// 5. Add one item to the end of the array. Log the array.
+
+const fruits = ["Apple", "Banana", "Mango"];
+
+fruits[fruits.length] = 'Orange';
+
+console.log(fruits);
---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m15_task5.js
@@ -0,0 +1,7 @@
+// 5. Add one item to the end of the array. Log the array.
+
+const fruits = ["Apple", "Banana", "Mango"];
+
+fruits.push('Cherry');
+
+console.log(fruits);
