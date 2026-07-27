9fa1f282f9d3dd12341519905b17ee2723a2cd94
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-07-27 19:56:04 +0600

    Looped through the array using a for loop

---
+++ milestones/milestone-02-javascript-foundations-with-problem-solving-mindset/module-10-fundamental-concepts-of-array/m21_task11.js
@@ -0,0 +1,7 @@
+// 11. Loop through the array with a for loop and log each item.
+
+const fruits = ["Apple", "Banana", "Mango"];
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
