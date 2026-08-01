379ff17dbc7237db790909c984b7b4a100bde1c2
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-02 04:21:22 +0600

    Practiced var, let, and const initialization

---
+++ milestones/milestone-03-empowering-javascript-with-es6/module-16-es6-fundamentals/m01.09_var_let_const.js
@@ -0,0 +1,19 @@
+var age;
+age = 23;
+console.log(age);
+
+
+let name;
+name = 'Alice';
+console.log(name);
+
+
+
+const str = 'const must initialize';
+console.log(str);
+console.log();
+
+
+// const num;          // const must initialize
+// num = 100;
+// console.log(num);
