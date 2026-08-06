4c5caea757983e2aacc0f5f20dc6af175bd785ca
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-06 23:48:00 +0600

    Add equality operator examples

---
+++ milestones/milestone-03-empowering-javascript-with-es6/module-17-more-es6/m03.01-==-vs-===.js
@@ -0,0 +1,14 @@
+console.log(5 == '5');
+console.log(5 === '5');
+console.log();
+
+console.log(false == 0);
+console.log(false === 0);
+console.log();
+
+console.log(true == 1);
+console.log(true === 1);
+console.log();
+
+console.log(null == undefined);
+console.log(null === undefined);
