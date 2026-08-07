914c07ca2ce6ae22ebc99c6e3c7016a32b179ac4
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-07 18:45:39 +0600

    Add pre and post increment or decrement examples

---
+++ milestones/milestone-03-empowering-javascript-with-es6/module-17-more-es6/m06.08-pre-post-increment.js
@@ -0,0 +1,8 @@
+let a = 5;
+console.log('Post Increment:', a++);      // 5
+console.log('Post Increment After:', a);    // 6
+
+
+let b = 10;
+console.log('Pre Increment:', ++b);     // 11
+console.log('Pre Increment After:', b);         // 11
---
+++ milestones/milestone-03-empowering-javascript-with-es6/module-17-more-es6/m06.09-pre-post-decrement.js
@@ -0,0 +1,8 @@
+let a = 5;
+console.log('Post Decrement:', a--);      // 5
+console.log('Post Decrement After:', a);    // 4
+
+
+let b = 10;
+console.log('Pre Decrement:', --b);     // 9
+console.log('Pre Decrement After:', b);         // 9
