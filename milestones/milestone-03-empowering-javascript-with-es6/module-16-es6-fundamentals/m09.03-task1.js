fae526b1a885399dd0f170f066b8ea733424dd98
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-09 19:48:51 +0600

    Implement declaration scope checker

---
+++ milestones/milestone-03-empowering-javascript-with-es6/module-16-es6-fundamentals/m09.03-task1.js
@@ -0,0 +1,21 @@
+const describeDeclaration = (keyword) => {
+    if(keyword === 'var') {
+        return 'Can redeclare, can reassign';
+    } else if(keyword === 'let') {
+        return 'Cannot redeclare, can reassign';
+    } else if(keyword === 'const') {
+        return 'Cannot redeclare, cannot reassign';
+    } else {
+        return 'Invalid';
+    }
+};
+
+console.log(describeDeclaration('let'));
+console.log(describeDeclaration('const'));
+console.log(describeDeclaration('var'));
+console.log(describeDeclaration('int'));
+console.log(describeDeclaration([10, 20, 30]));
+console.log(describeDeclaration([]));
+console.log(describeDeclaration({}));
+console.log(describeDeclaration(undefined));
+console.log(describeDeclaration(null));
