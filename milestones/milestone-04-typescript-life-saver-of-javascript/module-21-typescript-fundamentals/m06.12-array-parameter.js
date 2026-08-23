a938f245cec17014467aa017dea723a2dab6eedd
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-23 09:33:48 +0600

    Add TypeScript array parameter example

---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.12-array-parameter.js
@@ -0,0 +1,12 @@
+"use strict";
+// Array as Function Input
+const getTotal = (numbers) => {
+    let total = 0;
+    for (const number of numbers) {
+        total += number;
+    }
+    return total;
+};
+const result8 = getTotal([10, 20, 30]);
+console.log(result8);
+console.log(getTotal([1, 2, 3, 4, 5]));
