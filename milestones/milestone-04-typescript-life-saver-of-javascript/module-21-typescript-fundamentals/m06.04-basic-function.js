ef8696b6d6d9c6554daecfed5b246a843e45cc74
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-23 06:57:39 +0600

    Add TypeScript function parameter and return types

---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.04-basic-function.js
@@ -0,0 +1,16 @@
+"use strict";
+// JavaScript
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(10, 20));
+// TypeScript
+// Basic Function with Parameter Type
+function add(a, b) {
+    return a + b;
+}
+const result = add(10, 30);
+console.log(result);
+const ans = add(100, 200);
+console.log(ans);
+console.log(add(120, 130));
---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.04-basic-function.ts
@@ -0,0 +1,23 @@
+// JavaScript
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// console.log(add(10, 20));
+
+
+
+// TypeScript
+// Basic Function with Parameter Type
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+const result = add(10, 30);
+console.log(result);
+
+const ans: number = add(100, 200);
+console.log(ans);
+
+console.log(add(120, 130));
