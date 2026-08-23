cb5f78ac5329045b78c5e1aeceb0726ad011ef9a
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-23 07:49:32 +0600

    Add TypeScript no-parameter function example

---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.08-no-parameter.js
@@ -0,0 +1,15 @@
+"use strict";
+// Function with No Parameter
+// function getMessage(): string {
+//     return 'Welcome to TypeScript';
+// }
+// console.log(getMessage());
+const getMessage = () => {
+    return 'Welcome to TypeScript';
+};
+const result5 = getMessage();
+console.log(result5);
+const getMessage1 = () => {
+    return 'Welcome to TypeScript';
+};
+console.log(getMessage1());
---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.08-no-parameter.ts
@@ -0,0 +1,24 @@
+// Function with No Parameter
+
+// function getMessage(): string {
+//     return 'Welcome to TypeScript';
+// }
+
+// console.log(getMessage());
+
+
+
+const getMessage = () => {
+    return 'Welcome to TypeScript';
+}
+
+const result5 = getMessage();
+console.log(result5);
+
+
+
+const getMessage1 = (): string => {
+    return 'Welcome to TypeScript';
+}
+
+console.log(getMessage1());
