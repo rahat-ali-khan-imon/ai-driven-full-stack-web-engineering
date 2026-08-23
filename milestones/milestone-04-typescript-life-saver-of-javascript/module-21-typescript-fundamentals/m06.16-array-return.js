ab0ae1ff4e95d3dd0d8551ec7c4c11bf563afcb5
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-23 10:44:08 +0600

    Add TypeScript array return type example

---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.16-array-return.js
@@ -0,0 +1,6 @@
+"use strict";
+// Function Returning an Array
+const getNumbers = () => {
+    return [10, 20, 30, 40, 50];
+};
+console.log(getNumbers());
---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-21-typescript-fundamentals/m06.16-array-return.ts
@@ -0,0 +1,7 @@
+// Function Returning an Array
+
+const getNumbers = (): number[] => {
+    return [10, 20, 30, 40, 50];
+};
+
+console.log(getNumbers());
