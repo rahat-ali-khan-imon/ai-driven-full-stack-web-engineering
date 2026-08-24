f576940c8352a30e6ec6c9ec2d9b0027dee485ae
Md. Rahat Ali Khan Imon <rahatalikhanimon@gmail.com>
2026-08-24 10:25:36 +0600

    Add TypeScript optional property example

---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-22-more-typescript/dist/m02.04-optional-property.d.ts
@@ -0,0 +1,2 @@
+export {};
+//# sourceMappingURL=m02.04-optional-property.d.ts.map
---
+++ milestones/milestone-04-typescript-life-saver-of-javascript/module-22-more-typescript/dist/m02.04-optional-property.js
@@ -0,0 +1,22 @@
+"use strict";
+// Optional Property
+Object.defineProperty(exports, "__esModule", { value: true });
+const user1 = {
+    name: 'Alisa',
+    age: 23,
+    email: 'alisa@gmail.com'
+};
+const user2 = {
+    name: 'Alice',
+    age: 63 // email remove
+};
+console.log(user1);
+console.log(user1.name);
+console.log(user1.age);
+console.log(user1.email);
+console.log();
+console.log(user2);
+console.log(user2.name);
+console.log(user2.age);
+console.log(user2.email);
+//# sourceMappingURL=m02.04-optional-property.js.map
